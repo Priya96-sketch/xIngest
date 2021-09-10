eb2bd4ee0cbc91917491576d19258ab0319ee363
Priya Mondal <62085086+Priya96-sketch@users.noreply.github.com>
2021-09-10 19:56:12 +0530

    Update NavbarElements.js

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -29,10 +29,10 @@ max-width: 1100px;
 `
 
 export const NavLogo = styled(LinkR)`
-color:#2F54EB;
+color:#08979C;
 justify-self: flex-start;
 cursor: pointer;
-font-size: 1.5rem;
+font-size: 2rem;
 font-family: Open Sans;
 font-style: normal;
 font-weight: bold;
@@ -40,6 +40,7 @@ display: flex;
 align-itens: center;
 margin-left: 50px;
 margin-top: 28px;
+margin-right: 40px;
 font-weight: bold;
 text-decoration: none;
 `;
@@ -83,6 +84,7 @@ display: flex;
 align-items: center;
 text-align: left;
 margin-right: 20px;
+margin-top: 7px;
 margin-left: 10px;
 text-decoration: none;
 pudding: 0 1rem;
@@ -93,3 +95,34 @@ cursor: pointer;
     border-bottom: 3px solid #0bf71;
 }
 `;
+
+export const NavBtn = styled.nav`
+display: flex;
+align-items: center;
+
+@media screen and (max-width: 768px)
+{
+    display:none;
+}
+`
+export const NavBtnLink = styled(LinkR)`
+border-radius: 50px;
+background: #08979C;
+white—space: nowrap;
+padding: 10px 22px;
+color: #010606;
+font-size: 16px;
+outline: none;
+margin-left: 400px;
+border: none;
+cursor: pointer;
+transition: all 0.25 ease-in-out;
+text—decoration: none;
+
+&:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+}
+`;
+
